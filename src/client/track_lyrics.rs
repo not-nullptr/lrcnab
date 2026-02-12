8ad16bb9bc55922e3027e232a19cd8e34bcfac30
not-nullptr <nullptr@vert.sh>
2026-02-12 03:36:59 +0000

    fix: lyrics can be null!

--- src/client/track_lyrics.rs
+++ src/client/track_lyrics.rs
@@ -3,12 +3,14 @@ use serde::Deserialize;
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TrackLyrics {
-    pub plain_lyrics: String,
+    pub plain_lyrics: Option<String>,
     pub synced_lyrics: Option<String>,
 }
 
 impl TrackLyrics {
     pub fn lyrics(&self) -> &str {
-        self.synced_lyrics.as_deref().unwrap_or(&self.plain_lyrics)
+        self.synced_lyrics
+            .as_deref()
+            .unwrap_or(self.plain_lyrics.as_deref().unwrap_or(""))
     }
 }
